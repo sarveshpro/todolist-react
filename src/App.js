6108e6c2dcc9330373dbd8199fc86d9bb87f6837
Sarvesh Warge <sarveshwarge@gmail.com>
2020-10-03 10:51:40 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const inputRef = React.useRef(null);
+  const [list, addList] = React.useState(["Apple", "Banana"]);
+  const [printList, addPrintList] = React.useState(null);
+
+  function AddItem() {
+    if (inputRef.current !== null) {
+      if (
+        inputRef.current.value !== "" &&
+        inputRef.current.value !== undefined
+      ) {
+        addList(list.concat(inputRef.current.value));
+      }
+    }
+  }
+
+  React.useEffect(() => {
+    addPrintList(() => {
+      return list.map((listItem) => {
+        return (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <li
+              onClick={(event) => {
+                if (event.target.style.background !== "red") {
+                  event.target.style.background = "red";
+                } else {
+                  event.target.style.background = "#eee";
+                }
+              }}
+            >
+              {listItem}
+            </li>
+            <p
+              style={{ background: "#eee", padding: "0 20px", height: "auto" }}
+            >
+              X
+            </p>
+          </div>
+        );
+      });
+    });
+  }, [list]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Container">
+        <div>My TODO List</div>
+        <input ref={inputRef} type="text" />
+        <input
+          type="button"
+          onClick={() => {
+            AddItem();
+          }}
+          value="Add"
+        />
+      </div>
+      <div className="list">
+        <ul>{printList}</ul>
+      </div>
     </div>
   );
 }
